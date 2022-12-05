e0252e6654b739f7f35af0b4da01875411f049ff
DeepPlaying <1067676535@qq.com>
2022-12-05 15:08:06 +0800

    intergrate

--- visual_people.py
+++ visual_people.py
@@ -40,8 +40,6 @@ def visual_coordinates():
     XY[..., 1] += 280 # y
     return XY
 
-
-
 def displayboard_people(you_are_black=False, time_limit=5):
     pygame.init()
     # 设置主屏窗口
@@ -165,9 +163,75 @@ def displayboard_people(you_are_black=False, time_limit=5):
 
     return
 
+def displayBoard():
+    pygame.init()
+    # 设置主屏窗口
+    screen = pygame.display.set_mode((640,560))
+    # 设置窗口的标题，即游戏名称
+    pygame.display.set_caption('HexWuZi')
+    verts = visual_coordinates()
+    all_verts = []
 
+    for i in verts:
+        for j in i:
+            all_verts.append(j)
+
+    screen_color= '#E7B941'
+    line_color = '#000000'
+    screen.fill(screen_color)
+    display_board = np.zeros((12, 12), dtype=int)
+    display_board[0, 1:] = np.arange(0, 11)
+    display_board[1:, 0] = np.arange(0, 11)
+    broad = empty_broad.copy()
+    broad[5, 5] = 1
+    state = HexWuZiState(broad, -1)
+    display_board[1:, 1:] = broad
+    print(display_board)
+    searcher = MCTS(time_limit=5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        screen.fill(screen_color)
+        for i in all_verts:
+            for j in all_verts:
+                if sum((i-j)**2) < 3900:
+                    pygame.draw.line(screen,line_color,i,j,2)
+
+        for i in range(len(state.board)):
+            for j in range(len(state.board)):
+                if state.board[i][j] == 1:
+                    pygame.draw.circle(screen, "#000000",all_verts[11*i+j], 18,0)
+                    pygame.draw.circle(screen, "#808080",all_verts[11*i+j], 18,1)
+                if state.board[i][j] == -1:
+                    pygame.draw.circle(screen, "#FFFFFF",all_verts[11*i+j], 18,0)
+                    pygame.draw.circle(screen, "#808080",all_verts[11*i+j], 18,1)
+        pygame.display.flip()
+
+        currentPlayer = state.player
+        print(f"AI:{currentPlayer} is searching...")
+        action, detail = searcher.search(state, need_details=True)
+        print(action, detail)
+        state = state.take_action(action)
+        display_board[1:, 1:] = state.board
+        print(display_board)
+        winner, gameover = check(state.board)
+        if gameover:
+            if winner == currentPlayer:
+                print(f"AI:{currentPlayer} win!")
+                return
+            break
+
+def main(you_are_black=False, opponent_name = ""):
+    if opponent_name == "visual_people":
+        displayBoard()        
+    else:
+        displayboard_people(you_are_black=you_are_black)
+        
 if __name__ == "__main__":
-    displayboard_people(you_are_black=False)
+    main(you_are_black=False,opponent_name="visual_people")
 
 
 
